6a509d5d590bec89a9dcf694ce4a8d366aed36de
Phúc <nhbphuc2006@gmail.com>
2026-02-02 20:08:31 +0700

    fix: loading images 2

--- frontend/src/types/InterfaceGlobal.ts
+++ frontend/src/types/InterfaceGlobal.ts
@@ -3,62 +3,62 @@ import { Clock, HeartPulse, ShieldCheck, Van } from "lucide-react";
 export const brandSupport = [
   {
     name: "Bio Pharmercy",
-    image: "/images/bio.png",
+    image: "images/bio.png",
     path: "/store",
   },
   {
     name: "Catchy",
-    image: "/images/catchy.png",
+    image: "images/catchy.png",
     path: "/store",
   },
   {
     name: "Dr kyan",
-    image: "/images/drkyan.png",
+    image: "images/drkyan.png",
     path: "/store",
   },
   {
     name: "Ecopets",
-    image: "/images/ecopets.png",
+    image: "images/ecopets.png",
     path: "/store",
   },
   {
     name: "Ganador",
-    image: "/images/ganador.png",
+    image: "images/ganador.png",
     path: "/store",
   },
   {
     name: "Hanvet",
-    image: "/images/hanvet.png",
+    image: "images/hanvet.png",
     path: "/store",
   },
   {
     name: "Meow Cat",
-    image: "/images/meowcat.png",
+    image: "images/meowcat.png",
     path: "/store",
   },
   {
     name: "Minino",
-    image: "/images/minino.png",
+    image: "images/minino.png",
     path: "/store",
   },
   {
     name: "Moochie",
-    image: "/images/moochie.png",
+    image: "images/moochie.png",
     path: "/store",
   },
   {
     name: "Orgo",
-    image: "/images/orgo.png",
+    image: "images/orgo.png",
     path: "/store",
   },
   {
     name: "Vemedim",
-    image: "/images/vemedim.png",
+    image: "images/vemedim.png",
     path: "/store",
   },
   {
     name: "Wanpy",
-    image: "/images/wanpy.png",
+    image: "images/wanpy.png",
     path: "/store",
   },
 ];
